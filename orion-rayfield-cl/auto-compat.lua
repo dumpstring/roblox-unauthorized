4a762cbc400d4d24aba06d8c9a7c72a712364be1
dumpstring <100595391+dumpstring@users.noreply.github.com>
2024-11-28 02:37:36 +0100

    Update auto-compat.lua

--- orion-rayfield-cl/auto-compat.lua
+++ orion-rayfield-cl/auto-compat.lua
@@ -7,11 +7,9 @@ getgenv().loadstring = function(...)
 		or loading
 			== "https://raw.githubusercontent.com/darkkcontrol/Roblox-Orion-UI-Libary-OP-UI-LIBARY-/refs/heads/main/source"
 	then
-		return function()
-			return load(
-				"https://raw.githubusercontent.com/dumpstring/roblox-unauthorized/refs/heads/main/orion-rayfield-cl/layer.lua"
-			)
-		end
+		return load(
+			"https://raw.githubusercontent.com/dumpstring/roblox-unauthorized/refs/heads/main/orion-rayfield-cl/layer.lua"
+		)
 	end
 
 	return load(...)
